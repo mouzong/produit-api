d26e7eaecbb4fb9e71650736d3a932ce6a8e11df
Andreas MOUZONG <andreasmouzong@gmail.com>
2024-09-29 16:40:22 +0200

    Ajout des endpoint au CRUD

--- src/main/java/com/produit/produitservice/service/ProduitService.java
+++ src/main/java/com/produit/produitservice/service/ProduitService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +21,43 @@ public class ProduitService {
 
     public Produit createProduit(Produit produit) {
         return produitRepository.save(produit);
+    }
+
+    public Produit getProduitById(long id) {
+        Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+        if (optionalProduit.isEmpty()){
+            throw new RuntimeException("Desole produit inexistant");
+        }
+
+        return optionalProduit.get();
+    }
+
+    public String deleteProduitById(long idProduit) {
+        Optional<Produit> optionalProduit = produitRepository.findById(idProduit);
+
+        if (optionalProduit.isEmpty()){
+            throw new RuntimeException("Suppression impossible: PRODUIT INEXISTANT");
+        }
+
+        produitRepository.delete(optionalProduit.get());
+
+        return "Produit supprimé avec succès !";
+    }
+
+    public Produit editProduit(long id, Produit produit) {
+        Optional<Produit> optionalProduit = produitRepository.findById(id);
+
+        if (optionalProduit.isEmpty()){
+            throw new RuntimeException("Modification impossible !");
+        }
+
+        Produit produitAModifier = optionalProduit.get();
+
+        produitAModifier.setName(produit.getName());
+        produitAModifier.setPrice(produit.getPrice());
+
+        return produitRepository.save(produitAModifier);
+
     }
 }
